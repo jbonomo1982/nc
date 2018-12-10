fb9ba4925f4c3d3febed18ba53e051ac25ea2015
juan <j.m.bonomo1982@gmail.com>
2018-12-10 02:19:15 -0300

    Segundo Commit

--- nc/views.py
+++ nc/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import nC
 
 # Create your views here.
+
+def index(request):
+    nc = nC.objects.all().count()
+    return render(request, 'nc/index.html', {'nc':nc})
